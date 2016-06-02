1d8768508b5e0e183a6e232fafd614af443b569d
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-06-02 15:51:40 -0500

    basic config constants

--- config/config.php
+++ config/config.php
@@ -6,3 +6,8 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree or translated in the assets folder.
  */
+define('DB_CONNECTION','myconn.server.com:port');
+define('DB_NAME','dbName');
+define('DB_USER','dbUser');
+define('DB_PASSWORD','dbPass');
+define('SITE_ROOT', '/var/www/html/exposure');
