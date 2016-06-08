52257b4407c2115d017fdb48162202e9125a5bb5
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-06-07 23:59:11 -0500

    login method

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
  * LICENSE file in the root directory of this source tree or translated in the assets folder.
  */
 require_once 'config/config.php';
-//require_once 'lib/database.php';
+require_once 'lib/database.php';
+require_once 'lib/hash.php';
 require 'vendor/autoload.php';
 
 $app = new \Slim\App();
@@ -24,6 +25,49 @@ $app->get('/login', function ($request, $response, $args) {
     return $response;
 });
 
+$app->post('/login', function ($request, $response){
+    # Get body data
+    $data = $request->getParsedBody();
+
+    # Email Variables
+    # postData
+    $email      = filter_var($data['email'],    FILTER_SANITIZE_EMAIL);
+    $password   = filter_var($data['password'],       FILTER_SANITIZE_STRING);
+    $passwordReturned = "";
+    $messageResult = "";
+
+    # Create a MySQL connecton
+    $db = new Database();
+    $columns = array(
+        "ID",
+        "user_login",
+        "user_pass",
+        "display_name"
+    );
+    $where = "user_login = '$email'";
+    $user = $db->fetch('wp_users', $columns, $where);
+
+    # Now check the password stored with password hashed
+    $passwordReturned = $user["user_pass"];
+
+    $hasher = new PasswordHash(8, TRUE);
+    if($hasher->CheckPassword($password, $passwordReturned)) {
+        $messageResult = "Welcome " . $user["display_name"];
+    } else {
+        $messageResult = "Error invalid username or incorrect password";
+    }
+
+    unset($db);
+
+    $data = array(
+        "result"    => true,
+        "id"        => $user["ID"],
+        "email"     => $user["user_login"],
+        "message"   => $messageResult
+    );
+
+    $response->withJson($data);
+});
 
 // ================================================================
 // EMAIL
